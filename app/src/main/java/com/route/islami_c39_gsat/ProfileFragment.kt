f2b8ebe3cfd5d7f4724866cd13b344868329843f
Abdallah El-Zoka <abdallah.elzoka@gmail.com>
2024-01-20 10:13:20 +0200

    Feature2 - profile - finished working on profile Feature

--- app/src/main/java/com/route/islami_c39_gsat/ProfileFragment.kt
+++ app/src/main/java/com/route/islami_c39_gsat/ProfileFragment.kt
@@ -1,23 +1,27 @@
 package com.route.islami_c39_gsat
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.route.islami_c39_gsat.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+    lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        Log.e("TAG", "onViewCreated:  Finished Profile Screen")
     }
 }
