5c2bac576ef840e81d620212b20622a226be7e53
Abdallah El-Zoka <abdallah.elzoka@gmail.com>
2024-01-20 10:19:20 +0200

    Feature1 - home - finished working on home Feature

--- app/src/main/java/com/route/islami_c39_gsat/HomeFragment.kt
+++ app/src/main/java/com/route/islami_c39_gsat/HomeFragment.kt
@@ -1,23 +1,27 @@
 package com.route.islami_c39_gsat
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.route.islami_c39_gsat.databinding.FragmentHomeBinding
 
 // class HomeActivity : AppCompatActivity()
 class HomeFragment : Fragment() {
+    lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        Log.e("Tag", "Finished Develop Home Fragment")
     }
 }
