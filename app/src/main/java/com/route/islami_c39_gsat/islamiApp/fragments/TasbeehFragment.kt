9e988237830e4fff925b8fe9b8a61bcc9fc68925
0xFid0 <yfayad680@gmail.com>
2024-01-23 13:41:20 +0200

    finished TasbeehFragment and activity

--- app/src/main/java/com/route/islami_c39_gsat/islamiApp/fragments/TasbeehFragment.kt
+++ app/src/main/java/com/route/islami_c39_gsat/islamiApp/fragments/TasbeehFragment.kt
@@ -5,17 +5,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.route.islami_c39_gsat.databinding.FragmentQuranBinding
 import com.route.islami_c39_gsat.databinding.FragmentTasbeehBinding
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
 
 class TasbeehFragment : Fragment() {
     lateinit var binding: FragmentTasbeehBinding
+    private lateinit var sbeha: ImageView
+    private lateinit var tasbeehButton: Button
+    private lateinit var tasbeehCounter: TextView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentTasbeehBinding.inflate(inflater)
+        tasbeehCounter= binding.counterTasbeh
+        sbeha = binding.bodySbeeh
+        tasbeehButton = binding.tasbeh
+
+        var count=0
+        var counttext=0
+        tasbeehButton.setOnClickListener {
+            rotateImage()
+            count++
+            tasbeehCounter.text= count.toString()
+
+            if (count==33){
+                count=0
+                counttext++
+                tasbeehCounter.clearComposingText()
+                changeTextTasbeh(counttext)
+
+                if (counttext==3){
+                    counttext=0
+                    changeTextTasbeh(counttext)
+                }
+            }
+        }
         return binding.root
     }
 
@@ -23,5 +52,22 @@ class TasbeehFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
     }
+    private fun rotateImage() {
+        val currentRotation = sbeha.rotation
+        val newRotation = currentRotation + 10.9f
+        sbeha.animate().rotation(newRotation).start()
+    }
+    private fun changeTextTasbeh(counttext: Int) {
+        when(counttext){
+            0 -> tasbeehButton.text="سبحان الله"
 
-}
+            1 -> tasbeehButton.text="الحمد لله"
+
+            2 -> tasbeehButton.text="الله أكبر"
+
+            else -> {}
+            }
+        }
+    }
+
+
